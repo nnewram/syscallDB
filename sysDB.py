c9367cad5c19cd37029649dc4cc7bfb7675f43cc
marwenn02 <marwenn02@gmail.com>
2020-05-11 16:20:26 +0200

    moving to pip, added arm64 pickle. Refactored sysDB

--- sysDB.py
+++ sysDB.py
@@ -1,18 +1,23 @@
-def _syscallLookup(key, bitwidth, scraped=""):
+def _syscallLookup(key, arch, scraped=""):
     if scraped == "":
         if "pickle" not in locals():
             import pickle
-        if bitwidth == 32:
+        
+        if arch == 32:
             with open("syscallLookup/scrapedx64.pickle", "rb") as scrapeHandle:
                 scraped = pickle.load(scrapeHandle)
-            reverse = {v[1]: k for k, v in scraped.items()}
-        else:
+        
+        elif arch == 64:
             with open("syscallLookup/scrapedx86.pickle", "rb") as scrapeHandle:
                 scraped = pickle.load(scrapeHandle)
-            reverse = {v[0]: k for k, v in scraped.items()}
+        
+        elif arch == "arm64":
+            with open("syscallLookup/scrapedArm64.pickle", "rb") as scrapeHandle:
+                scraped = pickle.load(scrapeHandle)
     
     collected = {}
-    if bitwidth == 32:
+    
+    if arch == 32:
         collected["syscall"] = scraped[key][1]
         collected["eax"] = key
         collected["ebx"] = scraped[key][4]
@@ -20,7 +25,8 @@ def _syscallLookup(key, bitwidth, scraped=""):
         collected["edx"] = scraped[key][6]
         collected["esi"] = scraped[key][7]
         collected["edi"] = scraped[key][8]
-    else:
+    
+    elif arch == 64:
         try:
             collected["syscall"] = scraped[key][0]
             collected["rax"] = key
@@ -32,29 +38,51 @@ def _syscallLookup(key, bitwidth, scraped=""):
             collected["r9"] = scraped[key][6]
         except:
             pass
+    
+    elif arch == "arm64":
+        collected["syscall"] = scraped[key][0]
+        collected["x8"] = scraped[key][1]
+        collected["x0"] = scraped[key][2]
+        collected["x1"] = scraped[key][3]
+        collected["x2"] = scraped[key][4]
+        collected["x3"] = scraped[key][5]
+        collected["x4"] = scraped[key][6]
+        collected["x5"] = scraped[key][7]
+
     return collected
-def _reverseLookup(string, bitwidth):
+
+def _reverseLookup(string, arch):
     if "pickle" not in locals():
         import pickle
 
-    if bitwidth == 32:
+    if arch == 32:
         with open("syscallLookup/scrapedx64.pickle", "rb") as scrapeHandle:
             scraped = pickle.load(scrapeHandle)
         reverse = {v[1]: k for k, v in scraped.items()}
-    else:
+    
+    elif arch == 64:
         with open("syscallLookup/scrapedx86.pickle", "rb") as scrapeHandle:
+            scraped = pickle.load(scrapeHandle)
+        reverse = {v[0]: k for k, v in scraped.items()}
+    
+    elif arch == "arm64":
+        with open("syscallLookup/scrapedArm64.pickle", "rb") as scrapeHandle:
             scraped = pickle.load(scrapeHandle)
         reverse = {v[0]: k for k, v in scraped.items()}
     
     syscalls = []
     key = 0
+    reverseMax = max(scraped)
 
     for x in reverse:
+        while key not in scraped and key < reverseMax:
+            key += 1 # index might skip sometimes
+
         if string in x.lower():
-            syscalls.append(_syscallLookup(key, bitwidth, scraped))
-            key += 1
-            continue
+            syscalls.append(_syscallLookup(key, arch, scraped))
+        
         key += 1
+
     return syscalls
 
 def syscall32(query="sys"):
@@ -69,4 +97,11 @@ def syscall64(query):
         return _reverseLookup(query, 64)
     elif type(query) == int:
         return _syscallLookup(query, 64)
+    raise Exception("Invalid type: " + str(type(query)))
+
+def syscallarm64(query):
+    if type(query) == str:
+        return _reverseLookup(query, "arm64")
+    elif type(query) == int:
+        return _syscallLookup(query, "arm64")
     raise Exception("Invalid type: " + str(type(query)))
