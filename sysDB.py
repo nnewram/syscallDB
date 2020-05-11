b8d0da671cd89733f229825a17e47778c0d32fbd
marwenn02 <marwenn02@gmail.com>
2020-05-11 18:19:00 +0200

    added powerpc

--- sysDB.py
+++ sysDB.py
@@ -14,40 +14,55 @@ def _syscallLookup(key, arch, scraped=""):
         elif arch == "arm64":
             with open("syscallLookup/scrapedArm64.pickle", "rb") as scrapeHandle:
                 scraped = pickle.load(scrapeHandle)
+        
+        elif arch == "powerPC":
+            with open("syscallLookup/scrapedpowerpc.pickle", "rb") as scrapeHandle:
+                scraped = pickle.load(scrapeHandle)
     
     collected = {}
     
+    selectedSyscall = scraped[key]
     if arch == 32:
-        collected["syscall"] = scraped[key][1]
+        collected["syscall"] = selectedSyscall[1]
         collected["eax"] = key
-        collected["ebx"] = scraped[key][4]
-        collected["ecx"] = scraped[key][5]
-        collected["edx"] = scraped[key][6]
-        collected["esi"] = scraped[key][7]
-        collected["edi"] = scraped[key][8]
+        collected["ebx"] = selectedSyscall[4]
+        collected["ecx"] = selectedSyscall[5]
+        collected["edx"] = selectedSyscall[6]
+        collected["esi"] = selectedSyscall[7]
+        collected["edi"] = selectedSyscall[8]
     
     elif arch == 64:
         try:
-            collected["syscall"] = scraped[key][0]
+            collected["syscall"] = selectedSyscall[0]
             collected["rax"] = key
-            collected["rdi"] = scraped[key][1]
-            collected["rsi"] = scraped[key][2]
-            collected["rdx"] = scraped[key][3]
-            collected["r10"] = scraped[key][4]
-            collected["r8"] = scraped[key][5]
-            collected["r9"] = scraped[key][6]
+            collected["rdi"] = selectedSyscall[1]
+            collected["rsi"] = selectedSyscall[2]
+            collected["rdx"] = selectedSyscall[3]
+            collected["r10"] = selectedSyscall[4]
+            collected["r8"] = selectedSyscall[5]
+            collected["r9"] = selectedSyscall[6]
         except:
             pass
     
     elif arch == "arm64":
-        collected["syscall"] = scraped[key][0]
-        collected["x8"] = scraped[key][1]
-        collected["x0"] = scraped[key][2]
-        collected["x1"] = scraped[key][3]
-        collected["x2"] = scraped[key][4]
-        collected["x3"] = scraped[key][5]
-        collected["x4"] = scraped[key][6]
-        collected["x5"] = scraped[key][7]
+        collected["syscall"] = selectedSyscall[0]
+        collected["x8"] = selectedSyscall[1]
+        collected["x0"] = selectedSyscall[2]
+        collected["x1"] = selectedSyscall[3]
+        collected["x2"] = selectedSyscall[4]
+        collected["x3"] = selectedSyscall[5]
+        collected["x4"] = selectedSyscall[6]
+        collected["x5"] = selectedSyscall[7]
+
+    elif arch == "powerPC":
+        collected["syscall"] = selectedSyscall[0]
+        collected["r0"] = key
+        collected["r3"] = selectedSyscall[1]
+        collected["r4"] = selectedSyscall[2]
+        collected["r5"] = selectedSyscall[3]
+        collected["r6"] = selectedSyscall[4]
+        collected["r7"] = selectedSyscall[5]
+        collected["r8"] = selectedSyscall[6]
 
     return collected
 
@@ -70,6 +85,11 @@ def _reverseLookup(string, arch):
             scraped = pickle.load(scrapeHandle)
         reverse = {v[0]: k for k, v in scraped.items()}
     
+    elif arch == "powerPC":
+        with open("syscallLookup/scrapedpowerpc.pickle", "rb") as scrapeHandle:
+            scraped = pickle.load(scrapeHandle)
+        reverse = {v[0]: k for k, v in scraped.items()}
+
     syscalls = []
     key = 0
     reverseMax = max(scraped)
@@ -104,4 +124,11 @@ def syscallarm64(query):
         return _reverseLookup(query, "arm64")
     elif type(query) == int:
         return _syscallLookup(query, "arm64")
+    raise Exception("Invalid type: " + str(type(query)))
+
+def syscallPPC(query):
+    if type(query) == str:
+        return _reverseLookup(query, "powerPC")
+    elif type(query) == int:
+        return _syscallLookup(query, "powerPC")
     raise Exception("Invalid type: " + str(type(query)))
---
+++ syscallLookup/scrape/powerpc/syscallScrapePowerpc.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import pickle
+
+url = "https://syscalls.w3challs.com/?arch=powerpc_64"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+syscalls = soup.findAll("tr")[2:]
+sysdict = {}
+
+for syscall in syscalls:
+    sysarr = [str(syscall).split("</td><td>")[1].split("</a>")[0].split(">")[-1]]
+    sysarr += str(syscall).split("</td><td>")[3:9]
+    sysarr = [x if "<a" not in x else x.split("</a>")[0].split(">")[-1] for x in sysarr]
+    sysarr = [x if x != "-" else "" for x in sysarr]
+    syscallnumber = int(str(syscall).split("<td>")[1].split("</td>")[0])
+    sysdict[syscallnumber] = sysarr
+
+for x in sysdict:
+    print(sysdict[x])
+
+with open("../../scrapedpowerpc.pickle", "wb") as pickleHandle:
+    pickle.dump(sysdict, pickleHandle, protocol=pickle.HIGHEST_PROTOCOL)
+print("done")
